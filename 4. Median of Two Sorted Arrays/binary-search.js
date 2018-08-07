7e9759ab1ee56554f448c370f9ce0043cff98745
Yuri Predborskiy <shini1984@gmail.com>
2018-08-07 16:37:50 +0300

    Adapt a new algorithm, supposedly faster but memory-inefficient (tests don't show any significant difference, though).
    Idea - push items into new array as you go over the two existing arrays, up to half of all elements. Return last element or half of sum of last two elements.
    
    Code clean up.
    
    More fiddles in my jsfiddle page:
    https://jsfiddle.net/user/Shinigami84/fiddles/

--- 4. Median of Two Sorted Arrays/binary-search.js
+++ 4. Median of Two Sorted Arrays/binary-search.js
@@ -2,55 +2,36 @@
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
- * @param {boolean} logEnabled
  * @return {number}
  */
-let findMedianSortedArrays = function(nums1, nums2, logEnabled) {
-    function log(...rest) {
-        if (!logEnabled) return;
-        console.log(rest.join(' '));
-    }
-    log('input', nums1, nums2);
-    let len = (nums1.length + nums2.length - 1) / 2;
-    let range = [0, 1];
-    if (len === 0) {
-        range = [0];
-    } else if (len >= 1) {
-        if (len % 1 === 0 || len === 1) {
-            range = [len];
+let findMedianSortedArrays = function(nums1, nums2) {
+    // new algorithm, memory inefficient but faster
+    // idea - push half of elements into array, return last element
+    // or half of sum of last two elements (if number of elements is even)
+    let total = nums1.length + nums2.length;
+    log('inputs', nums1, nums2);
+    log('total is', total);
+
+    let left = 0, right = 0, res = [];
+    for (let i = 0; i <= Math.floor(total / 2); i++) {
+        if (left === nums1.length || nums1[left] > nums2[right]) {
+            log('adding right', right);
+            res[i] = nums2[right++];
         } else {
-            log('len is', len);
-            range = [Math.floor(len), Math.ceil(len)];
+            log('adding left', left);
+            res[i] = nums1[left++];
         }
     }
 
-    log('range is', range);
-
-    let left = 0, right = 0, last = 0, res = [];
-    while (range.length) {
-        log('last is', last);
-        if (last === range[0]) {
-            log('found desired range.', 'left is', left, 'right is', right);
-            if (nums1[left] <= nums2[right] || !nums2[right]) {
-                res.push(nums1[left++]);
-            } else if (nums1[left] > nums2[right] || !nums1[left]) {
-                res.push(nums2[right++]);
-            }
-            range.shift();
-        } else if (nums1[left] <= nums2[right] || right > nums2.length - 1) {
-            log('range not found, upping left', nums1[left], 'right', nums2[right]);
-            left++;
-        } else if (nums1[left] > nums2[right] || left > nums1.length - 1) {
-            log('range not found, upping right');
-            right++;
-        }
-        last++;
-        if (last > nums1.length + nums2.length) throw Error('input size limit exceeded');
-    }
-
-    return res.length > 1 ? (res[0] + res[1]) / 2 : res[0];
+    return total % 2 === 0 ? (res[res.length - 1] + res[res.length - 2]) / 2 : res[res.length - 1];
 };
 
+function log(...rest) {
+    if (!logEnabled) return;
+    console.log(rest.join(' '));
+}
+
+let logEnabled = false;
 let tests = [
     { a: [1], b: [2], ans: 1.5 },
     { a: [1, 3], b: [2], ans: 2 },
@@ -65,7 +46,9 @@ let tests = [
 tests.forEach((item) => {
     let res = findMedianSortedArrays(item.a, item.b);
     if (res !== item.ans) {
-        console.log('expected', item.ans, 'calculated', findMedianSortedArrays(item.a, item.b, true));
+        logEnabled = true;
+        console.log('expected', item.ans, 'calculated', findMedianSortedArrays(item.a, item.b));
+        logEnabled = false;
     }
 });
 
@@ -88,7 +71,10 @@ The median is (2 + 3)/2 = 2.5
 
 /*
 
-Shorter version
+old solution - slow but memory-efficient
+idea:
+iterate over array till you find middle
+find middle by using left and right indexes and last index (total number of processed elements)
 
 var findMedianSortedArrays = function(nums1, nums2) {
     let len = (nums1.length + nums2.length - 1) / 2;
