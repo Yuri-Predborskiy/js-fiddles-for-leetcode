85d488c3bb7d121e2fbe2ccfb2fd033c71a24aa1
Yuri Predborski <shini1984@gmail.com>
2018-08-13 17:52:20 +0300

    Add O(log(n)) very verbose solution. Uses binary search. Add lots of logging to understand how it all works in detail.
    Credit goes to GeeksForGeeks.org article:
    https://www.geeksforgeeks.org/k-th-element-two-sorted-arrays/
    
    Thanks to Satish and Aditya Kamath for working C# implementation here:
    https://ide.geeksforgeeks.org/DJu7to

---
+++ 4. Median of Two Sorted Arrays/ologn.js
@@ -0,0 +1,80 @@
+let number = 0;
+
+function kth(arr1, arr2, start1, end1, start2, end2, k) {
+    let mid1 = Math.floor(Math.abs(start1 - end1) / 2);
+    let mid2 = Math.floor(Math.abs(start2 - end2) / 2);
+
+    console.log('try', ++number, 'start1', start1, 'end1', end1, 'start2', start2, 'end2', end2, 'k', k);
+
+    if (start1 === end1) {
+        console.log('reached end of array 1, element not found, so it should be at index start2 + k in arr2');
+        console.log(`looking at arr2[${start2 + k}], value is ${arr2[start2 + k]}`);
+        return arr2[start2 + k];
+    }
+    if (start2 === end2) {
+        console.log('reached end of array 2, element not found, so it should be at index start1 + k in arr1');
+        console.log(`looking at arr1[${start1 + k}], value is ${arr1[start1 + k]}`);
+        return arr1[start1 + k];
+    }
+
+    if (mid1 + mid2 < k) {
+        // console.log(`mid1 (${mid1}) + mid2 (${mid2}) < k (${k})`);
+        console.log(`mids (${mid1} + ${mid2} = ${mid1 + mid2}) < k (${k}), lets increase start and limit k`);
+        if (arr1[start1 + mid1] < arr2[start2 + mid2]) {
+            // console.log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} < arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
+            console.log('limiting start of left and limiting k - mid1 - 1');
+            return kth(arr1, arr2, start1 + mid1 + 1, end1, start2, end2, k - mid1 - 1);
+        } else {
+            // console.log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} >= arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
+            console.log('limiting start of right and limiting k - mid2 - 1');
+            return kth(arr1, arr2, start1, end1, start2 + mid2 + 1, end2, k - mid2 - 1);
+        }
+    } else {
+        // console.log(`mid1 (${mid1}) + mid2 (${mid2}) >= k (${k})`);
+        console.log(`mids (${mid1} + ${mid2} = ${mid1 + mid2}) >= k ${k}, lets limit end`);
+        if (arr1[start1 + mid1] < arr2[start2 + mid2]) {
+            // console.log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} < arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
+            console.log('limiting end of right');
+            return kth(arr1, arr2, start1, end1, start2, start2 + mid2, k);
+        } else {
+            // console.log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} >= arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
+            console.log('limiting end of left');
+            return kth(arr1, arr2, start1, start1 + mid1, start2, end2, k);
+        }
+    }
+}
+
+let tests = [
+    {
+        a: [0, 1, 2, 3, 6, 7, 9],
+        b: [12, 14, 18, 20, 21, 22, 23, 24, 25],
+        k: 6,
+        ans: 7
+    },
+    {
+        a: Array.from(new Array(100),(val, index) => index + 101),
+        b: Array.from(new Array(100),(val, index) => index + 1),
+        k: 100,
+        ans: 100
+    },
+    {
+        a: Array.from(new Array(100),(val, index) => index + 1),
+        b: Array.from(new Array(100),(val, index) => index + 101),
+        k: 100,
+        ans: 100
+    },
+    {
+        a: Array.from(new Array(100),(val, index) => index * 2 + 1),
+        b: Array.from(new Array(100),(val, index) => index * 2 + 2),
+        k: 49,
+        ans: 49
+    }
+];
+
+tests.forEach(test => {
+    let res = kth(test.a, test.b, 0, test.a.length, 0, test.b.length, test.k - 1);
+    console.log('Inputs:', 'array1', JSON.stringify(test.a), '| array2', JSON.stringify(test.b), '| index', test.k, '| answer', test.ans);
+    console.log('expected:', test.ans, '| calculated:', res, '| result is', res === test.ans ? 'CORRECT' : 'WRONG!');
+    number = 0;
+    console.log('');
+});
