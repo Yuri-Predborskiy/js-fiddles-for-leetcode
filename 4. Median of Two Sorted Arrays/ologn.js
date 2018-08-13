6b6a169bdef3d2b43287e9c561b727c522fe608f
Yuri Predborski <shini1984@gmail.com>
2018-08-13 18:00:22 +0300

    Make logging less verbose.

--- 4. Median of Two Sorted Arrays/ologn.js
+++ 4. Median of Two Sorted Arrays/ologn.js
@@ -1,44 +1,50 @@
 let number = 0;
+let logging = false;
+let logs = 0;
+
+function log(...messages) {
+    if (logging) console.log(messages.join(' '));
+}
 
 function kth(arr1, arr2, start1, end1, start2, end2, k) {
     let mid1 = Math.floor(Math.abs(start1 - end1) / 2);
     let mid2 = Math.floor(Math.abs(start2 - end2) / 2);
 
-    console.log('try', ++number, 'start1', start1, 'end1', end1, 'start2', start2, 'end2', end2, 'k', k);
+    log('try', ++number, 'start1', start1, 'end1', end1, 'start2', start2, 'end2', end2, 'k', k);
 
     if (start1 === end1) {
-        console.log('reached end of array 1, element not found, so it should be at index start2 + k in arr2');
-        console.log(`looking at arr2[${start2 + k}], value is ${arr2[start2 + k]}`);
+        log('reached end of array 1, element not found, so it should be at index start2 + k in arr2');
+        log(`looking at arr2[${start2 + k}], value is ${arr2[start2 + k]}`);
         return arr2[start2 + k];
     }
     if (start2 === end2) {
-        console.log('reached end of array 2, element not found, so it should be at index start1 + k in arr1');
-        console.log(`looking at arr1[${start1 + k}], value is ${arr1[start1 + k]}`);
+        log('reached end of array 2, element not found, so it should be at index start1 + k in arr1');
+        log(`looking at arr1[${start1 + k}], value is ${arr1[start1 + k]}`);
         return arr1[start1 + k];
     }
 
     if (mid1 + mid2 < k) {
-        // console.log(`mid1 (${mid1}) + mid2 (${mid2}) < k (${k})`);
-        console.log(`mids (${mid1} + ${mid2} = ${mid1 + mid2}) < k (${k}), lets increase start and limit k`);
+        // log(`mid1 (${mid1}) + mid2 (${mid2}) < k (${k})`);
+        log(`mids (${mid1} + ${mid2} = ${mid1 + mid2}) < k (${k}), lets increase start and limit k`);
         if (arr1[start1 + mid1] < arr2[start2 + mid2]) {
-            // console.log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} < arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
-            console.log('limiting start of left and limiting k - mid1 - 1');
+            // log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} < arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
+            log('limiting start of left and limiting k - mid1 - 1');
             return kth(arr1, arr2, start1 + mid1 + 1, end1, start2, end2, k - mid1 - 1);
         } else {
-            // console.log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} >= arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
-            console.log('limiting start of right and limiting k - mid2 - 1');
+            // log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} >= arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
+            log('limiting start of right and limiting k - mid2 - 1');
             return kth(arr1, arr2, start1, end1, start2 + mid2 + 1, end2, k - mid2 - 1);
         }
     } else {
-        // console.log(`mid1 (${mid1}) + mid2 (${mid2}) >= k (${k})`);
-        console.log(`mids (${mid1} + ${mid2} = ${mid1 + mid2}) >= k ${k}, lets limit end`);
+        // log(`mid1 (${mid1}) + mid2 (${mid2}) >= k (${k})`);
+        log(`mids (${mid1} + ${mid2} = ${mid1 + mid2}) >= k ${k}, lets limit end`);
         if (arr1[start1 + mid1] < arr2[start2 + mid2]) {
-            // console.log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} < arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
-            console.log('limiting end of right');
+            // log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} < arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
+            log('limiting end of right');
             return kth(arr1, arr2, start1, end1, start2, start2 + mid2, k);
         } else {
-            // console.log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} >= arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
-            console.log('limiting end of left');
+            // log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} >= arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
+            log('limiting end of left');
             return kth(arr1, arr2, start1, start1 + mid1, start2, end2, k);
         }
     }
@@ -73,8 +79,12 @@ let tests = [
 
 tests.forEach(test => {
     let res = kth(test.a, test.b, 0, test.a.length, 0, test.b.length, test.k - 1);
-    console.log('Inputs:', 'array1', JSON.stringify(test.a), '| array2', JSON.stringify(test.b), '| index', test.k, '| answer', test.ans);
-    console.log('expected:', test.ans, '| calculated:', res, '| result is', res === test.ans ? 'CORRECT' : 'WRONG!');
+    logging = res !== test.ans;
+    if (logging) logs++;
+    kth(test.a, test.b, 0, test.a.length, 0, test.b.length, test.k - 1);
+    log('Inputs:', 'array1', JSON.stringify(test.a), '| array2', JSON.stringify(test.b), '| index', test.k, '| answer', test.ans);
+    log('expected:', test.ans, '| calculated:', res, '| result is', res === test.ans ? 'CORRECT' : 'WRONG!');
     number = 0;
-    console.log('');
+    log('');
 });
+if (!logs) console.log('all done');
