d8a5cb7902e7dcfd67a63d3aa48a2a9eadfcf0f4
Yuri Predborski <shini1984@gmail.com>
2018-08-14 11:39:31 +0300

    update short version.

--- 4. Median of Two Sorted Arrays/ologn-short.js
+++ 4. Median of Two Sorted Arrays/ologn-short.js
@@ -1,25 +1,25 @@
-function kth(arr1, arr2, start1, end1, start2, end2, k) {
-    let mid1 = Math.floor(Math.abs(start1 - end1) / 2);
-    let mid2 = Math.floor(Math.abs(start2 - end2) / 2);
+function kth(arrLeft, arrRight, index, startLeft = 0, endLeft = arrLeft.length, startRight = 0, endRight = arrRight.length) {
+    let shiftLeft = Math.floor((endLeft - startLeft) / 2);
+    let shiftRight = Math.floor((endRight - startRight) / 2);
 
-    if (start1 === end1) {
-        return arr2[start2 + k];
+    if (startLeft === endLeft) {
+        return arrRight[startRight + index];
     }
-    if (start2 === end2) {
-        return arr1[start1 + k];
+    if (startRight === endRight) {
+        return arrLeft[startLeft + index];
     }
 
-    if (mid1 + mid2 < k) {
-        if (arr1[start1 + mid1] < arr2[start2 + mid2]) {
-            return kth(arr1, arr2, start1 + mid1 + 1, end1, start2, end2, k - mid1 - 1);
+    if (shiftLeft + shiftRight < index) {
+        if (arrLeft[startLeft + shiftLeft] < arrRight[startRight + shiftRight]) {
+            return kth(arrLeft, arrRight, index - shiftLeft - 1, startLeft + shiftLeft + 1, endLeft, startRight, endRight);
         } else {
-            return kth(arr1, arr2, start1, end1, start2 + mid2 + 1, end2, k - mid2 - 1);
+            return kth(arrLeft, arrRight, index - shiftRight - 1, startLeft, endLeft, startRight + shiftRight + 1, endRight);
         }
     } else {
-        if (arr1[start1 + mid1] < arr2[start2 + mid2]) {
-            return kth(arr1, arr2, start1, end1, start2, start2 + mid2, k);
+        if (arrLeft[startLeft + shiftLeft] < arrRight[startRight + shiftRight]) {
+            return kth(arrLeft, arrRight, index, startLeft, endLeft, startRight, startRight + shiftRight);
         } else {
-            return kth(arr1, arr2, start1, start1 + mid1, start2, end2, k);
+            return kth(arrLeft, arrRight, index, startLeft, startLeft + shiftLeft, startRight, endRight);
         }
     }
 }
@@ -28,31 +28,37 @@ let tests = [
     {
         a: [0, 1, 2, 3, 6, 7, 9],
         b: [12, 14, 18, 20, 21, 22, 23, 24, 25],
-        k: 6,
+        index: 6,
         ans: 7
     },
     {
         a: Array.from(new Array(100),(val, index) => index + 101),
         b: Array.from(new Array(100),(val, index) => index + 1),
-        k: 100,
+        index: 100,
         ans: 100
     },
     {
         a: Array.from(new Array(100),(val, index) => index + 1),
         b: Array.from(new Array(100),(val, index) => index + 101),
-        k: 100,
+        index: 100,
         ans: 100
     },
     {
         a: Array.from(new Array(100),(val, index) => index * 2 + 1),
         b: Array.from(new Array(100),(val, index) => index * 2 + 2),
-        k: 49,
+        index: 49,
         ans: 49
+    },
+    {
+        a: [1, 2, 5, 17, 28],
+        b: [3, 4, 7, 9],
+        index: 6,
+        ans: 7
     }
 ];
 
 tests.forEach(test => {
-    let res = kth(test.a, test.b, 0, test.a.length, 0, test.b.length, test.k - 1);
-    console.log('Inputs:', 'array1', JSON.stringify(test.a), '| array2', JSON.stringify(test.b), '| index', test.k, '| answer', test.ans);
+    let res = kth(test.a, test.b, test.index - 1);
+    console.log('Inputs:', 'array1', JSON.stringify(test.a), '| array2', JSON.stringify(test.b), '| index', test.index, '| answer', test.ans);
     console.log('expected:', test.ans, '| calculated:', res, '| result is', res === test.ans ? 'CORRECT' : 'WRONG!');
 });
