a8e3dfd740a1bbd5c210847e8e26ba6d58141647
Yuri Predborski <shini1984@gmail.com>
2018-08-14 11:37:46 +0300

    Optimize code: rename variables for readability, add parameter defaults (to limit the number of parameters required to launch this function).

--- 4. Median of Two Sorted Arrays/ologn.js
+++ 4. Median of Two Sorted Arrays/ologn.js
@@ -1,4 +1,4 @@
-let number = 0;
+let pass = 0;
 let logging = false;
 let logs = 0;
 
@@ -6,46 +6,46 @@ function log(...messages) {
     if (logging) console.log(messages.join(' '));
 }
 
-function kth(arr1, arr2, start1, end1, start2, end2, k) {
-    let mid1 = Math.floor(Math.abs(start1 - end1) / 2);
-    let mid2 = Math.floor(Math.abs(start2 - end2) / 2);
+function kth(arrLeft, arrRight, index, startLeft = 0, endLeft = arrLeft.length, startRight = 0, endRight = arrRight.length) {
+    let shiftLeft = Math.floor((endLeft - startLeft) / 2);
+    let shiftRight = Math.floor((endRight - startRight) / 2);
 
-    log('try', ++number, 'start1', start1, 'end1', end1, 'start2', start2, 'end2', end2, 'k', k);
+    log('pass', pass++, 'startLeft', startLeft, 'endLeft', endLeft, 'startRight', startRight, 'endRight', endRight, 'index', index);
 
-    if (start1 === end1) {
-        log('reached end of array 1, element not found, so it should be at index start2 + k in arr2');
-        log(`looking at arr2[${start2 + k}], value is ${arr2[start2 + k]}`);
-        return arr2[start2 + k];
+    if (startLeft === endLeft) {
+        log('reached end of array 1, element not found, so it should be at index startRight + index in arrRight');
+        log(`looking at arrRight[${startRight + index}], value is ${arrRight[startRight + index]}`);
+        return arrRight[startRight + index];
     }
-    if (start2 === end2) {
-        log('reached end of array 2, element not found, so it should be at index start1 + k in arr1');
-        log(`looking at arr1[${start1 + k}], value is ${arr1[start1 + k]}`);
-        return arr1[start1 + k];
+    if (startRight === endRight) {
+        log('reached end of array 2, element not found, so it should be at index startLeft + index in arrLeft');
+        log(`looking at arrLeft[${startLeft + index}], value is ${arrLeft[startLeft + index]}`);
+        return arrLeft[startLeft + index];
     }
 
-    if (mid1 + mid2 < k) {
-        // log(`mid1 (${mid1}) + mid2 (${mid2}) < k (${k})`);
-        log(`mids (${mid1} + ${mid2} = ${mid1 + mid2}) < k (${k}), lets increase start and limit k`);
-        if (arr1[start1 + mid1] < arr2[start2 + mid2]) {
-            // log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} < arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
-            log('limiting start of left and limiting k - mid1 - 1');
-            return kth(arr1, arr2, start1 + mid1 + 1, end1, start2, end2, k - mid1 - 1);
+    if (shiftLeft + shiftRight < index) {
+        // log(`shiftLeft (${shiftLeft}) + shiftRight (${shiftRight}) < index (${index})`);
+        log(`mids (${shiftLeft} + ${shiftRight} = ${shiftLeft + shiftRight}) < index (${index}), lets increase start and limit index`);
+        if (arrLeft[startLeft + shiftLeft] < arrRight[startRight + shiftRight]) {
+            // log(`arrLeft[startLeft + shiftLeft] ${arrLeft[startLeft + shiftLeft]} < arrRight[startRight + shiftRight] ${arrRight[startRight + shiftRight]}`);
+            log('limiting start of left and limiting index - shiftLeft - 1');
+            return kth(arrLeft, arrRight, index - shiftLeft - 1, startLeft + shiftLeft + 1, endLeft, startRight, endRight);
         } else {
-            // log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} >= arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
-            log('limiting start of right and limiting k - mid2 - 1');
-            return kth(arr1, arr2, start1, end1, start2 + mid2 + 1, end2, k - mid2 - 1);
+            // log(`arrLeft[startLeft + shiftLeft] ${arrLeft[startLeft + shiftLeft]} >= arrRight[startRight + shiftRight] ${arrRight[startRight + shiftRight]}`);
+            log('limiting start of right and limiting index - shiftRight - 1');
+            return kth(arrLeft, arrRight, index - shiftRight - 1, startLeft, endLeft, startRight + shiftRight + 1, endRight);
         }
     } else {
-        // log(`mid1 (${mid1}) + mid2 (${mid2}) >= k (${k})`);
-        log(`mids (${mid1} + ${mid2} = ${mid1 + mid2}) >= k ${k}, lets limit end`);
-        if (arr1[start1 + mid1] < arr2[start2 + mid2]) {
-            // log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} < arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
+        // log(`shiftLeft (${shiftLeft}) + shiftRight (${shiftRight}) >= index (${index})`);
+        log(`mids (${shiftLeft} + ${shiftRight} = ${shiftLeft + shiftRight}) >= index ${index}, lets limit end`);
+        if (arrLeft[startLeft + shiftLeft] < arrRight[startRight + shiftRight]) {
+            // log(`arrLeft[startLeft + shiftLeft] ${arrLeft[startLeft + shiftLeft]} < arrRight[startRight + shiftRight] ${arrRight[startRight + shiftRight]}`);
             log('limiting end of right');
-            return kth(arr1, arr2, start1, end1, start2, start2 + mid2, k);
+            return kth(arrLeft, arrRight, index, startLeft, endLeft, startRight, startRight + shiftRight);
         } else {
-            // log(`arr1[start1 + mid1] ${arr1[start1 + mid1]} >= arr2[start2 + mid2] ${arr2[start2 + mid2]}`);
+            // log(`arrLeft[startLeft + shiftLeft] ${arrLeft[startLeft + shiftLeft]} >= arrRight[startRight + shiftRight] ${arrRight[startRight + shiftRight]}`);
             log('limiting end of left');
-            return kth(arr1, arr2, start1, start1 + mid1, start2, end2, k);
+            return kth(arrLeft, arrRight, index, startLeft, startLeft + shiftLeft, startRight, endRight);
         }
     }
 }
@@ -54,37 +54,46 @@ let tests = [
     {
         a: [0, 1, 2, 3, 6, 7, 9],
         b: [12, 14, 18, 20, 21, 22, 23, 24, 25],
-        k: 6,
+        index: 6,
         ans: 7
     },
     {
         a: Array.from(new Array(100),(val, index) => index + 101),
         b: Array.from(new Array(100),(val, index) => index + 1),
-        k: 100,
+        index: 100,
         ans: 100
     },
     {
         a: Array.from(new Array(100),(val, index) => index + 1),
         b: Array.from(new Array(100),(val, index) => index + 101),
-        k: 100,
+        index: 100,
         ans: 100
     },
     {
         a: Array.from(new Array(100),(val, index) => index * 2 + 1),
         b: Array.from(new Array(100),(val, index) => index * 2 + 2),
-        k: 49,
+        index: 49,
         ans: 49
+    },
+    {
+        a: [1, 2, 5, 17, 28],
+        b: [3, 4, 7, 9],
+        index: 6,
+        ans: 5
     }
 ];
 
 tests.forEach(test => {
-    let res = kth(test.a, test.b, 0, test.a.length, 0, test.b.length, test.k - 1);
+    let res = kth(test.a, test.b, test.index - 1);
     logging = res !== test.ans;
-    if (logging) logs++;
-    kth(test.a, test.b, 0, test.a.length, 0, test.b.length, test.k - 1);
-    log('Inputs:', 'array1', JSON.stringify(test.a), '| array2', JSON.stringify(test.b), '| index', test.k, '| answer', test.ans);
+    if (logging) {
+        logs++;
+        pass = 0;
+        kth(test.a, test.b, test.index - 1); // repeat procedure with logging enabled
+        logging = !logging;
+    }
+    log('Inputs:', 'array1', JSON.stringify(test.a), '| array2', JSON.stringify(test.b), '| index', test.index, '| answer', test.ans);
     log('expected:', test.ans, '| calculated:', res, '| result is', res === test.ans ? 'CORRECT' : 'WRONG!');
-    number = 0;
     log('');
 });
 if (!logs) console.log('all done');
