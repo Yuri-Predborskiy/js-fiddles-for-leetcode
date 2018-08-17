63506f0c8241b3bedddea7edb1d167cb4e4aa8e1
Yuri Predborski <shini1984@gmail.com>
2018-08-17 14:00:09 +0300

    Add while loop solution. Refactor recursive solution to add a wrapper function that validates inputs (so that inputs are validated once instead of being validated on every recursive call).

--- 50. Pow(x, n)/binary-search.js
+++ 50. Pow(x, n)/binary-search.js
@@ -4,18 +4,46 @@
  * @return {number}
  */
 let myPow = function(x, n) {
-    if (n === 0 || x === 1) return 1;
+    function pow(x, n) {
+        if (n === 0) return 1;
+        if (n % 3 === 0) {
+            return pow(x * x * x, n / 3);
+        } else if (n % 2 === 0) {
+            return pow(x * x, n / 2);
+        } else {
+            return x * pow(x, --n);
+        }
+    }
+
+    if (x === 1) return 1;
     if (n < 0) {
         n = -n;
         x = 1/x;
     }
-    if (n % 3 === 0) {
-        return myPow(x * x * x, n / 3);
-    } else if (n % 2 === 0) {
-        return myPow(x * x, n / 2);
-    } else {
-        return x * myPow(x, --n);
+    return pow(x, n);
+};
+
+// using a while loop
+let myPow2 = function(x, n) {
+    if (x === 1 || n === 0) return 1;
+
+    if (n < 0) {
+        n = -n;
+        x = 1/x;
     }
+
+    let res = 1;
+
+    while (n > 0) {
+        if (n % 2 === 1) {
+            res *= x;
+            --n;
+        }
+        x *= x;
+        n /= 2;
+    }
+
+    return res;
 };
 
 let tests = [
@@ -31,6 +59,6 @@ let tests = [
 ];
 
 tests.forEach(test => {
-    let res = Math.round(myPow(test.x, test.n)*100000)/100000;
+    let res = Math.round(myPow2(test.x, test.n)*100000)/100000;
     console.log('expected:', test.ans, '| calculated:', res, '| result is', res === test.ans ? 'CORRECT' : 'WRONG!');
 });
