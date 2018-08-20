b51db1571e7d25a4d9b59afd85d449191e567aaf
Yuri Predborski <shini1984@gmail.com>
2018-08-20 19:28:29 +0300

    Fix array comparison bug (always returns "correct").

--- 350. Intersection of Two Arrays II/binary-search.js
+++ 350. Intersection of Two Arrays II/binary-search.js
@@ -57,8 +57,8 @@ tests.forEach(test => {
 
 function compareArrays(arr1, arr2) {
     if (arr1.length !== arr2.length) return false;
-    arr1.forEach(item => {
-        if (!arr2.includes(item)) return false;
-    });
+    for (let i = 0; i < arr1.length; i++) {
+        if (!arr2.includes(arr1[i])) return false;
+    }
     return true;
 }
