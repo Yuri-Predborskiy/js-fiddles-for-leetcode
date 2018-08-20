88ef502ebafb859871a821f95b8c1ac0bb2b5d82
Yuri Predborski <shini1984@gmail.com>
2018-08-20 18:33:48 +0300

    Add hash table solution (faster).

---
+++ 350. Intersection of Two Arrays II/hash-table.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    let hash = {};
+    nums1.forEach(item => {
+        hash[item] = (hash[item] || 0) + 1;
+    });
+    let res = [];
+    nums2.forEach(item => {
+        if (!hash[item]) {
+            return;
+        }
+        hash[item]--;
+        res.push(item);
+    });
+
+    return res;
+};
+
+let tests = [
+    { nums1: [1,2,2,1], nums2: [2,2], ans: [2,2] },
+    { nums1: [4,9,5], nums2: [9,4,9,8,4], ans: [9,4] },
+    { nums1: [2,1], nums2: [1,2], ans: [1,2] },
+];
+
+tests.forEach(test => {
+    let res = intersect(test.nums1, test.nums2);
+    let correct = compareArrays(test.ans, res);
+    console.log('expected:', test.ans, '| calculated:', res, '| result is', correct ? 'CORRECT' : 'WRONG!');
+});
+
+function compareArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    arr1.forEach(item => {
+        if (!arr2.includes(item)) return false;
+    });
+    return true;
+}
