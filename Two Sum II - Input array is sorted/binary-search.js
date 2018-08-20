3886407c7303d3d55b1d5dacbbb3eee634613293
Yuri Predborski <shini1984@gmail.com>
2018-08-20 19:26:53 +0300

    Draft Two Sum II - Input array is sorted. Issue - need to increment left one at a time, while right is found using binary search (no way around it, one side needs to be checked one item at a time - in case of negative values)

---
+++ Two Sum II - Input array is sorted/binary-search.js
@@ -0,0 +1,71 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    function findNext(left, right, target, direction) {
+        while (left < right) {
+            let mid = Math.floor((left + right) / 2);
+            if (nums[mid] === target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        if (direction > 0) {
+            return (nums[right] > target && right + 1 < nums.length)  ? right + 1 : right;
+        } else {
+            return (nums[left] > target && left - 1 > 0) ? left - 1 : left;
+        }
+    }
+
+    let left = 0, right = nums.length - 1;
+    let cnt = 0;
+
+    while (nums[left] + nums[right] !== target) {
+        // in case of infinite cycle, break glass and throw error
+        cnt++; if (cnt > 900) { throw new Error('infinite cycle detected, exiting'); }
+
+        if (nums[left] + nums[right] === target) {
+            break;
+        } else if (nums[left] + nums[right] > target) {
+            right = findNext(left, right - 1, target - nums[left], -1);
+        } else {
+            left = findNext(left + 1, right, target - nums[right], 1);
+        }
+    }
+    return [left + 1, right + 1];
+};
+
+let tests = [
+    { nums: [2,7,11,15], target: 9, ans: [1,2] },
+    { nums: [0,1,2,3,4,5,7,11,15], target: 9, ans: [3,7] },
+    {
+        nums: [
+            12,13,23,28,43,44,59,60,61,68,70,86,88,92,124,125,136,168,173,173,180,199,212,221,227,230,277,282,
+            306,314,316,321,325,328,336,337,363,365,368,370,370,371,375,384,387,394,400,404,414,422,422,427,430,
+            435,457,493,506,527,531,538,541,546,568,583,585,587,650,652,677,691,730,737,740,751,755,764,778,783,
+            785,789,794,803,809,815,847,858,863,863,874,887,896,916,920,926,927,930,933,957,981,997
+        ],
+        target: 542,
+        ans: [1,2]
+    }
+];
+
+tests.forEach(test => {
+    let res = twoSum(test.nums, test.target);
+    let correct = compareArrays(test.ans, res);
+    console.log('expected:', test.ans, '| calculated:', res, '| result is', correct ? 'CORRECT' : 'WRONG!');
+});
+
+function compareArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (!arr2.includes(arr1[i])) return false;
+    }
+    return true;
+}
