cc67fe0c126f2b333cf8dd9e8dac903a6cbdf17c
Yuri Predborski <shini1984@gmail.com>
2018-08-20 16:38:01 +0300

    Implement hash table solution.

--- Intersection of Two Arrays/hash-table.js
+++ Intersection of Two Arrays/hash-table.js
@@ -4,54 +4,45 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    // find intersection
-    // 2. hash table
-    // 1st array - hash with all values set to true
-    // iterate over 2nd array, for every element present in hash, set hash to false, push to result
-    // return result
-    // complexity O(mn) for iterating over both arrays + O(1) for picking elements
-
-    function makeHash(items) {
-        let set = {};
-        items.forEach(item => {
-            set[item] = true;
-        });
-        return set;
-    }
-
-    let hash = makeHash(nums1);
-
-
-    let left = 0, right = nums.length - 1;
-
-    while (left + 1 < right) {
-        let mid = Math.floor((left + right) / 2);
-        if (nums[mid] > nums[right]) {
-            left = mid;
-        } else if (nums[mid] < nums[left]) {
-            right = mid;
-        } else {
-            right--;
+    let hash = nums1.forEach(item => {
+        hash[item] = true;
+    });
+    let res = [];
+    nums2.forEach(item => {
+        if (!hash[item]) {
+            return;
         }
-    }
+        hash[item] = false;
+        res.push(item);
+    });
 
-    return Math.min(nums[left], nums[right]);
+    return res;
 };
 
 let tests = [
-    { nums: [3,4,5,1,2] , ans: 1 },
-    { nums: [4,5,6,7,0,1,2], ans: 0 },
-    { nums: [1], ans: 1 },
-    { nums: [5,1,2], ans: 1 },
-    { nums: [1,3,5], ans: 1 },
-    { nums: [2,2,2,0,1], ans: 0 },
-    { nums: [2,2,2,2,2,3,4,5,6,1], ans: 1 },
-    { nums: [4,1,4], ans: 1 },
-    { nums: [3,3,1,2,3,3,3,3,3,3,3], ans: 1 },
-    { nums: [3,3,1,3], ans: 1 },
+    { nums1: [1,2,2,1], nums2: [2,2], ans: [2] },
+    { nums1: [4,9,5], nums2: [9,4,9,8,4], ans: [9,4] },
+    {
+        nums1: [
+            61,24,20,58,95,53,17,32,45,85,70,20,83,62,35,89,5,95,12,86,58,77,30,64,46,13,5,92,67,40,20,38,31,
+            18,89,85,7,30,67,34,62,35,47,98,3,41,53,26,66,40,54,44,57,46,70,60,4,63,82,42,65,59,17,98,29,72,1,96,
+            82,66,98,6,92,31,43,81,88,60,10,55,66,82,0,79,11,81
+        ],
+        nums2: [5,25,4,39,57,49,93,79,7,8,49,89,2,7,73,88,45,15,34,92,84,38,85,34,16,6,99,0,2,36,68,52,73,50,77,44,61,48],
+        ans: [61,45,85,89,5,77,92,38,7,34,44,57,4,6,88,0,79]
+    }
 ];
 
 tests.forEach(test => {
-    let res = findMin(test.nums);
-    console.log('expected:', test.ans, '| calculated:', res, '| result is', res === test.ans ? 'CORRECT' : 'WRONG!');
+    let res = intersection(test.nums1, test.nums2);
+    let correct = compareArrays(test.ans, res);
+    console.log('expected:', test.ans, '| calculated:', res, '| result is', correct ? 'CORRECT' : 'WRONG!');
 });
+
+function compareArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    arr1.forEach(item => {
+        if (!arr2.includes(item)) return false;
+    });
+    return true;
+}
