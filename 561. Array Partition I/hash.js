8b971b0243a3792a430dd2b9b3993f13990c1cb2
Yuri Predborski <shini1984@gmail.com>
2018-08-31 11:45:23 +0300

    Update hash solution - fix min/max values bug and remove redundant code.

--- 561. Array Partition I/hash.js
+++ 561. Array Partition I/hash.js
@@ -1,49 +1,39 @@
+// todo: write proper explanation in a separate file
 /**
  * @param {number[]} nums
  * @return {number}
  */
-let arrayPairSumHanging = function(nums) {
+let arrayPairSum = function(nums) {
+    // create a hash table where array index = key (nums item), array value = number of times key repeats in nums
+    // purpose - to provide a pre-defined hash table that can be used in place of a sorted array
+    // sorting is O(n*log(n)), a slow operation
+    // iterating through the array of nums once and creating an index of values is O(n), faster
     let hash = [];
     for (let i = 0; i < 20001; ++i) {
         hash[i] = 0;
     }
 
-    let sum = 0, min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER;
+    let sum = 0, min = Infinity, max = -Infinity;
     for (let i = 0; i < nums.length; ++i) {
         let num = nums[i] + 10000;
         ++hash[num];
         min = Math.min(min, num);
         max = Math.max(max, num);
     }
-    let take = 0;
-    for(let i = min; i <= max; ++i){
-        let repeats = hash[i];
-        for(let j = 0; j < repeats; ++j) {
-            if (take == 0) {
-                sum += i - 10000;
-            }
-            take ^= 1;
-        }
-    }
-    return sum;
-};
 
-let arrayPairSumWorking = function(nums) {
-    let hash = Array(20000).fill(0), sum = 0, min = nums[0], max = nums[0];
-    for (let i = 0; i < nums.length; ++i) {
-        let num = nums[i] + 10000;
-        ++hash[num];
-        min = Math.min(min, num);
-        max = Math.max(max, num);
-    }
-    let take = 1;
-    for(let i = min; i <= max; ++i){
-        let repeats = hash[i];
-        for(let j = 0; j < repeats; ++j) {
+    // all values are "pairs", we need to take a smaller value of a pair but aim to maximize sum of pairs (sum of smaller values in pairs)
+    // we can achieve this by gathering values with minimum difference into pairs so that the smallest amount is lost in a pair
+    // this can be achieved via sorting (we achieved this via hash table instead of sorting, which is faster)
+    // in order to emulate pairs of values, we simply take every other value, and switch "take" on/off on every pass
+    let take = true;
+    for (let i = min; i <= max; ++i){
+        // if hash table value is 0, we didn't have this number in nums, so we shouldn't consider it
+        if (!hash[i]) continue;
+        for (let j = 0; j < hash[i]; ++j) {
             if (take) {
                 sum += i - 10000;
             }
-            take ^= 1;
+            take = !take;
         }
     }
     return sum;
