c5c9e10c3883991c0c775625599cb28e227a0f68
Yuri Predborski <shini1984@gmail.com>
2018-09-14 14:17:01 +0300

    add todo for global testing.

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 console.log("Hello world! All fiddles are in their respective folders");
+// todo: create a test suite that would run all the applications and output results of testing each individual file
