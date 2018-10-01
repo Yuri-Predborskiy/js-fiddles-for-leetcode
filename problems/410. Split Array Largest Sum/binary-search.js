4f14efe2ca53ba836b386ac0eab8b4ca60cf0b9a
Yuri Predborski <shini1984@gmail.com>
2018-10-01 17:21:56 +0300

    Add problem 410. Split Array Largest Sum. Solution using binary search to find number that satisfies the input problem.

--- problems/410. Split Array Largest Sum/binary-search.js
+++ problems/410. Split Array Largest Sum/binary-search.js
@@ -1,30 +1,75 @@
+/*
+    Binary search sum search:
+    instead of checking every possible sum, apply binary search to "find" sum
+    left: biggest element
+    right: sum of all elements
+
+    time complexity: O(n*log(n))
+    log(n) for binary search, n for reading the whole input array to see if it breaks into m pieces with specified sub sum
+ */
+
 /**
  * @param {number[]} nums
+ * @param {number} m
  * @return {number}
  */
-let findMin = function(nums) {
-    let left = 0, right = nums.length - 1;
-
-    while (left + 1 < right) {
-        let mid = Math.floor((left + right) / 2);
-        if (nums[mid] > nums[right]) {
-            left = mid;
-        } else {
-            right = mid;
+let splitArray = function(nums, m) {
+    function getMaxAndSum() {
+        for (let i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (nums[i] > max) {
+                max = nums[i];
+            }
         }
     }
 
-    return Math.min(nums[left], nums[right]);
+    function getNumberOfBreakpoints(limit) {
+        let breaks = 1, sum = 0;
+        for (let i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > limit) {
+                sum = nums[i];
+                breaks++;
+            }
+            if (breaks > m) {
+                break;
+            }
+        }
+        return breaks;
+    }
+
+    let max = 0, sum = 0;
+    getMaxAndSum();
+
+    if (m === 1) {
+        return sum;
+    }
+    if (m === nums.length) {
+        return max;
+    }
+
+    let left = max, right = sum, breaks;
+    while (left < right) {
+        let mid = Math.floor((right - left) / 2 + left); // prevents overflow
+        breaks = getNumberOfBreakpoints(mid);
+        if (breaks <= m) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return right;
 };
 
 let tests = [
-    { nums: [3,4,5,1,2] , ans: 1 },
-    { nums: [4,5,6,7,0,1,2], ans: 0 },
-    { nums: [1], ans: 1 },
-    { nums: [3,1,2], ans: 1 },
+    { nums: [10,5,13,4,8,4,5,11,14,9,16,10,20,8], m: 8, ans: 25 },
+    { nums: [7,2,5,10,8], m: 2, ans: 18 },
+    { nums: [1,2,3,4,5,6,7,8,9], m: 3, ans: 17 },
+    { nums: [1,2147483647], m: 2, ans: 2147483647 },
 ];
 
 tests.forEach(test => {
-    let res = findMin(test.nums);
+    let res = splitArray(test.nums, test.m);
     console.log('expected:', test.ans, '| calculated:', res, '| result is', res === test.ans ? 'CORRECT' : 'WRONG!');
 });
