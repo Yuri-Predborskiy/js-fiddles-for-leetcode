ae9a6304f4c8beeceb0eba107539dd78ee6d262f
Yuri Predborski <shini1984@gmail.com>
2018-10-08 16:54:24 +0300

    705. Design HashSet. Solutions using javascript object notation. Which is cheating, basically :D TODO: implement a different solution using primitive types and collections (like Array), without using javascript object notation (which is a type of built-in hash table type, which we are trying to recreate).

---
+++ problems/705. Design HashSet/object.js
@@ -0,0 +1,54 @@
+/**
+ * Initialize your data structure here.
+ */
+let MyHashSet = function() {
+    this.values = {};
+};
+
+/**
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function(key) {
+    this.values[key] = true;
+};
+
+/**
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function(key) {
+    this.values[key] = false;
+};
+
+/**
+ * Returns true if this set contains the specified element
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function(key) {
+    return this.values[key] || false;
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = Object.create(MyHashSet).createNew()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
+
+function check(checkValue, calculated) {
+    let res = checkValue === calculated ? 'CORRECT' : 'WRONG!';
+    console.log('should be', checkValue, '| calculated', calculated, '| result is', res);
+}
+
+let hashSet = new MyHashSet();
+hashSet.add(1);
+hashSet.add(2);
+check(hashSet.contains(1), true);
+check(hashSet.contains(3), false);
+hashSet.add(2);
+check(hashSet.contains(2), true);
+hashSet.remove(2);
+check(hashSet.contains(2), false);
