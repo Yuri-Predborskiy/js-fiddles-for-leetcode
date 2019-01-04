850413a09076117839a44053f151e0c21f127a56
Yuri Predborski <shini1984@gmail.com>
2019-01-04 12:46:30 +0200

    Add problem 232. Implement Queue using Stacks. Solution by reversing stack for certain operations (pop from stack to temp, this results in a reversed stack in temp, top of the reversed stack is the bottom of the original stack).

---
+++ problems/232. Implement Queue using Stacks/queue-using-stack.js
@@ -0,0 +1,77 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function() {
+    this.stack = [];
+};
+
+/**
+ * Reverse the stack, peek at the first element, removes it if necessary, put the stack back in the original order
+ * @param   {boolean}       bRemoveFirst
+ * @returns {number, null}
+ */
+MyQueue.prototype.getFirstElement = function(bRemoveFirst) {
+    if (this.empty()) {
+        return null;
+    }
+
+    let reversed = [];
+    while (!this.empty()) {
+        reversed.push(this.stack.pop());
+    }
+    let res = bRemoveFirst ? reversed.pop() : reversed[reversed.length - 1];
+    while (reversed.length > 0) {
+        this.stack.push(reversed.pop());
+    }
+    return res;
+};
+
+/**
+ * Push element x to the back of queue.
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack.push(x);
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    return this.getFirstElement(true);
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.getFirstElement();
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.stack.length === 0;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = Object.create(MyQueue).createNew()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
+
+let queue = new MyQueue();
+
+queue.push(1);
+queue.push(2);
+console.log('1', queue.peek());  // returns 1
+console.log('1', queue.pop());   // returns 1
+console.log('false', queue.empty()); // returns false
