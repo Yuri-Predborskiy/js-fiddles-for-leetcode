2499c89614768b8885bcd3a1ea1a0023b18bbe69
Yuri Predborski <shini1984@gmail.com>
2019-01-18 17:46:29 +0200

    Add todo.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("Hello world! All fiddles are in their respective folders");
 // todo: create a test suite that would run all the applications and output results of testing each individual file
 
+// todo: clean up this file and move recursive vs iterative approaches to tree traversal into a separate file
 // tree parsing using recursive approach
 let {TreeNode} = require('./problems/helper');
 
