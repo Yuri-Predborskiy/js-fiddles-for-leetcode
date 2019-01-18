6b7a3b6d3cbe90ba8bf5ef0f4f65741fe6cf28af
Yuri Predborski <shini1984@gmail.com>
2019-01-18 17:45:43 +0200

    Create algorithm to transform array into binary tree iteratively. Update recursive approach to make it fit into a self-sustained function (one that does not require outside index) via wrapper function.

--- index.js
+++ index.js
@@ -4,28 +4,57 @@ console.log("Hello world! All fiddles are in their respective folders");
 // tree parsing using recursive approach
 let {TreeNode} = require('./problems/helper');
 
-function calc(array) {
-    if (index >= array.length || array[index] === null) {
-        index++;
-        return;
+function transformArrayToTree(array) {
+    let index = 0;
+    function calc() {
+        if (index >= array.length || array[index] === null) {
+            index++;
+            return;
+        }
+        let root = new TreeNode(array[index++]);
+        let branches = ['left', 'right'];
+        for (let branch of branches) {
+            let temp = calc(array);
+            if (temp) root[branch] = temp;
+        }
+        return root;
     }
-    let root = new TreeNode(array[index++]);
-    let branches = ['left', 'right'];
-    for (let branch of branches) {
-        let temp = calc(array);
-        if (temp) root[branch] = temp;
-    }
-    return root;
+    return calc(array);
 }
-let index = 0;
 
 let arr1 = 'a,b,d,h,x,x,i,x,x,e,x,k,x,x,c,f,x,x,g'.split(',');
 arr1 = arr1.map(item => item === 'x' ? null : item);
 let arr2 = [1, null, 2, 3];
 
-let test = calc(arr1);
-let test2 = calc(arr2);
+let recursive1 = transformArrayToTree(arr1);
+let recursive2 = transformArrayToTree(arr2);
 
+function transform(array) {
+    function addBranch(root, branch) {
+        if (index >= array.length) return;
+        stack.push({root, branch});
+    }
+
+    if (!array.length) return null;
+
+    let tree = new TreeNode(array[0]), index = 0, stack = [];
+    // for LIFO / stack and binary tree, right branch goes first
+    addBranch(tree, 'right');
+    addBranch(tree, 'left');
+    // for FIFO and binary tree, left branch should go first
+
+    while (stack.length && ++index < array.length) {
+        let {root, branch} = stack.pop();
+        if (array[index] === null) continue;
+        root[branch] = new TreeNode(array[index]);
+        addBranch(root[branch], 'right');
+        addBranch(root[branch], 'left');
+    }
+    return tree;
+}
+
+let iterative1 = transform(arr1);
+let iterative2 = transform(arr2);
 // todo implement tree parsing via using iterative approach
 
 console.log('done');
