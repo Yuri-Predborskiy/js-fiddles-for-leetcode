d91fc0a9df577fd938844875888e2dca6770bd1a
Yuri Predborski <shini1984@gmail.com>
2020-05-20 12:55:59 +0200

    Add problem 230. Kth Smallest Element in a BST. Solution using iot via iteration.
    Note to self: do not use git commit amend command as it seems to commit ALL of the uncommited files instead of simply modifying last commit's message. Git is Weird.

--- problems/230. Kth Smallest Element in a BST/iot-iteration.js
+++ problems/230. Kth Smallest Element in a BST/iot-iteration.js
@@ -1,9 +1,13 @@
 /*
-Depth first search
-Dive into the tree looking at each level whether node satisfies conditions
+In-order traversal
+Drop through the left tree to the bottom. This is 1st smallest
+If k > 1, decrement k and return null
+if received null, check in-order traversal next node (left, root, right->left)
+if received non-null value, return that value
+Keep repeating checks and decrementing k till k = 1, then return value
 
-Time complexity: O(n), nodes are not revisited
-Space complexity: O(n) for stack, wost case
+Time complexity: O(n), in the worst case we'll visit all nodes to find the value, in best case it is O(height)
+Space complexity: O(1) outside of recursion stack
  */
 
 const {convertArrayToBinaryTreeLevelOrderTraversal} = require('../helper');
@@ -18,38 +22,51 @@ const {convertArrayToBinaryTreeLevelOrderTraversal} = require('../helper');
  */
 /**
  * @param {TreeNode} root
+ * @param {number} k
  * @return {number}
  */
-let goodNodes = function(root) {
-    if (!root) {
-        return 0;
-    }
+let kthSmallest = function(root, k) {
+    const stack = [root];
+    const visited = new Set();
+    let count = k;
 
-    const stack = [{node: root, max: root.val}];
-    let paths = 0;
     while (stack.length > 0) {
-        let {node, max} = stack.pop();
+        const node = stack.pop();
         if (!node) {
             continue;
         }
-        if (node.val >= max) {
-            paths++;
+
+        if (visited.has(node)) {
+            if (count === 1) {
+                return node.val;
+            }
+            count--;
+            stack.push(node.right);
+        } else {
+            stack.push(node);
+            stack.push(node.left);
+            visited.add(node);
         }
-        max = Math.max(node.val, max);
-        stack.push({node: node.left, max});
-        stack.push({node: node.right, max});
     }
-    return paths;
 };
 
 let tests = [
-    {params: [3,1,4,3,null,1,5], ans: 4},
-    {params: [3,3,null,4,2], ans: 3},
-    {params: [1], ans: 1},
+    {params: [[3,1,4,null,2], 1], ans: 1},
+    {params: [[5,3,6,2,4,null,null,1], 3], ans: 3},
+    {params: [[50,30,60,20,40,null,null,10,25,35,45,null,null,21], 1], ans: 10},
+    {params: [[50,30,60,20,40,null,null,10,25,35,45,null,null,21], 2], ans: 20},
+    {params: [[50,30,60,20,40,null,null,10,25,35,45,null,null,21], 3], ans: 21},
+    {params: [[50,30,60,20,40,null,null,10,25,35,45,null,null,21], 4], ans: 25},
+    {params: [[50,30,60,20,40,null,null,10,25,35,45,null,null,21], 5], ans: 30},
+    {params: [[50,30,60,20,40,null,null,10,25,35,45,null,null,21], 6], ans: 35},
+    {params: [[50,30,60,20,40,null,null,10,25,35,45,null,null,21], 7], ans: 40},
+    {params: [[50,30,60,20,40,null,null,10,25,35,45,null,null,21], 8], ans: 45},
+    {params: [[50,30,60,20,40,null,null,10,25,35,45,null,null,21], 9], ans: 50},
+    {params: [[50,30,60,20,40,null,null,10,25,35,45,null,null,21], 10], ans: 60},
 ];
 
 tests.forEach(test => {
-    let res = goodNodes(convertArrayToBinaryTreeLevelOrderTraversal(test.params));
+    let res = kthSmallest(convertArrayToBinaryTreeLevelOrderTraversal(test.params[0]), test.params[1]);
     let correct = res === test.ans;
     console.log(`expected: '${test.ans}' | calculated: '${res}' | result is`, correct ? 'CORRECT' : 'WRONG!');
 });
